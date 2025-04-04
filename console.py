d29decc3d4f35d1e00e3e7a55736ed87f5f17aab
derphilovic <philovic@outlook.de>
2025-04-04 19:42:13 +0200

    directory control

--- console.py
+++ console.py
@@ -5,13 +5,19 @@ import os
 i = 1
 s = 0
 c = 0
-commands =  [ 'psy', 'help', 'exit', 'version' ]
+commands =  [ 'psy', 'help', 'exit', 'version', 'clear', 'newdir', 'godir', 'listdir', 'homedir', 'deldir' ]
 commandslen = len(commands)
 commandinfo = {
     'psy' : 'psy - displays psy-cmd logo',
     'help' : 'help - displays all commands',
     'exit' : 'exit - exits psy-cmd',
-    'version' : 'version - displays the psy-cmd version'
+    'version' : 'version - displays the psy-cmd version',
+    'clear' : 'clear - clears the screen',
+    'newdir' : 'newdir - creates a new directory. Please don\'t enter the name of the dir with the command itself.',
+    'godir' : 'godir - goes to a directory. Please don\'t enter the name of the dir with the command itself.',
+    'listdir' : 'listdir - lists all directories and sub directories in the current directory',
+    'homedir' : 'homedir - goes to the home directory',
+    'deldir' : 'deldir - deletes a directory. Please don\'t enter the name of the dir with the command itself.',
 }
 
 logo = """                                                        __     
@@ -23,12 +29,14 @@ logo = """                                                        __
   \\ \\ \\/  \\/___/  `/___/> \\        \\/____/\\/_/\\/_/\\/_/\\/__,_ / 
    \\ \\_\\             /\\___/                                   
     \\/_/             \\/__/                                    """
+vers = "psy-cmd 0.0.1"
 #startup functions
 
 if not os.path.exists('psy-data'):
     os.mkdir('psy-data')
 
 os.chdir('psy-data')
+psy_data_root = os.getcwd()
 
 def get_truncated_path(start_folder):
     full_path = os.getcwd()
@@ -39,10 +47,13 @@ def get_truncated_path(start_folder):
 
 rootpath = get_truncated_path('psy-data')
 os.system('cls')
+print(vers)
+print(logo)
 #main functions
 while True:
     inputcmd = input("PSY:/" + rootpath + ">")
-
+    inputcmd = inputcmd.strip()
+    inputcmd = inputcmd.lower()
     while s < commandslen:
         if inputcmd == commands[s]:
             c = 1
@@ -58,19 +69,83 @@ while True:
 
     #help command
     if c == 1 and inputcmd == 'help':
-        print(commandinfo['psy'])
-        print(commandinfo['help'])
-        print(commandinfo['exit'])
-        print(commandinfo['version'])
+        for cmd in commands:
+            print(commandinfo[cmd])
+        c = 0
 
     #psy command
     if c == 1 and inputcmd == 'psy':
         print(logo)
+        c = 0
 
     #exit command
     if c == 1 and inputcmd == 'exit':
+        os.system('cls')
         exit()
 
     #version command
     if c == 1 and inputcmd == 'version':
-        print("psy-cmd v 0.0.1")
+        print(vers)
+        c = 0
+
+    #clear command
+    if c == 1 and inputcmd == 'clear':
+        os.system('cls')
+        c = 0
+    
+    #newdir command
+    if c == 1 and inputcmd == 'newdir':
+        newdir = input("newdir: ")
+        if os.path.exists(newdir):
+            print("directory already exists")
+        else:
+            os.mkdir(newdir)
+            print("new directory " + newdir + " created")
+        c = 0
+
+    #godir command
+    if c == 1 and inputcmd == 'godir':
+        targetdir = input("godir: ")
+        if os.path.exists(targetdir):
+            os.chdir(targetdir)
+            print("changed directory to " + targetdir)
+        else:
+            print("directory not found")
+    
+    #listdir command
+    if c == 1 and inputcmd == 'listdir':
+        items = os.listdir()
+        print("Contents of current directory:")
+        for item in items:
+            if os.path.isdir(item):
+                print(f"[DIR] {item}")
+                sub_items = os.listdir(item)
+                for sub_item in sub_items:
+                    path = os.path.join(item, sub_item)
+                    if os.path.isdir(path):
+                        print(f"  ├─[DIR] {sub_item}")
+                    else:
+                        print(f"  ├─[FILE] {sub_item}")
+            else:
+                print(f"[FILE] {item}")
+        c = 0
+
+    #homedir command
+    if c == 1 and inputcmd == 'homedir':
+        os.chdir(psy_data_root)
+        print("changed directory to " + get_truncated_path('psy-data'))
+        c = 0
+    
+    #deldir command
+    if c == 1 and input == 'deldir':
+        deldir = input("deldir: ")
+        if os.path.exists(deldir):
+            os.rmdir(deldir)
+            print("deleted directory " + deldir)
+        else:
+            print("directory not found")
+    
+
+    #reset variables
+    s = 0
+    rootpath = get_truncated_path('psy-data')
